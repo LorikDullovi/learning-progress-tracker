cfcbf77f443a12f87a8377350416b34078761fe6
LorikDullovi <lorikdullovi@gmail.com>
2026-02-05 00:07:19 +0100

    feat: Implement initial authentication routes and database connection with `bcryptjs` dependency.

---
+++ controllers/auth.controller.js
@@ -0,0 +1,34 @@
+const authService = require('../services/auth.service');
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password, age, profilePicture, role } = req.body;
+    const user = await authService.register(username, email, age, password, role, profilePicture);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExists = await authService.findUser(email);
+    if (!userExists) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const user = await authService.login(email, password);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
+
+
+
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ require('dotenv').config();
 const connectDB = require('./db/connection');
 connectDB();
 
+// Routes
+const authRoutes = require('./routes/auth.routes');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// API Routes
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
